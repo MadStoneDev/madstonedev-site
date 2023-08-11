6529286f426a77b421700c64b08278193aab1ef8
richardhaddadau <richard@haddads.net.au>
2023-08-11 14:28:17 +1000

    strapi added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,20 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const strapiConfig = {
+  apiURL: process.env.STRAPI_API_URL,
+  accessToken: process.env.STRAPI_TOKEN,
+  collectionTypes: ["article", "company", "author", "project"],
+  singleTypes: [],
+  remoteFileHeaders: {
+    Referer: "https://madstone.dev",
+  },
+};
+
 module.exports = {
   siteMetadata: {
     title: `MadStoneDev`,
@@ -19,6 +33,10 @@ module.exports = {
           head: true,
         },
       },
+    },
+    {
+      resolve: "gatsby-source-strapi",
+      options: strapiConfig,
     },
     "gatsby-plugin-postcss",
   ],
