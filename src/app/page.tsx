5e8cd7b772dacd77474bc18f546346ffad8f1e6f
Richard H. <richard@haddads.net.au>
2025-03-27 16:07:45 +1000

    fix for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,9 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <main className={`p-8 pb-20 gap-16 sm:p-20 font-sans text-white`}>
+    <main
+      className={`px-4 sm:px-8 py-5 sm:py-8 gap-16 sm:p-20 font-sans text-white`}
+    >
       <section className={`flex flex-row items-center gap-2`}>
         <img
           src={`/madstone-logo.svg`}
@@ -16,8 +18,12 @@ export default function Home() {
         />
 
         <article className={`flex flex-col`}>
-          <h1 className={`font-display text-4xl uppercase`}>MadStone Dev</h1>
-          <h2 className={`-mt-1.5 text-primary`}>
+          <h1 className={`font-display text-xl sm:text-4xl uppercase`}>
+            MadStone Dev
+          </h1>
+          <h2
+            className={`-mt-0.5 max-w-52 sm:max-w-lg sm:-mt-1.5 text-sm sm:text-base text-primary`}
+          >
             Perfecting the method, embracing the madness
           </h2>
         </article>
--- src/components/sections/section-projects.tsx
+++ src/components/sections/section-projects.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function SectionProjects() {
   return (
     <section
-      className={`my-10 flex flex-row items-stretch gap-4 w-full h-[600px] text-xl`}
+      className={`my-6 sm:my-10 flex flex-col sm:flex-row items-stretch gap-4 w-full sm:h-[600px] text-base sm:text-xl`}
     >
       {/*<div*/}
       {/*  className={`hover:flex-grow w-40 border transition-all duration-300`}*/}
@@ -13,7 +13,7 @@ export default function SectionProjects() {
       {/*  <Link href={`https://lfu-companion.vercel.app`}>LFU Companion</Link>*/}
       {/*</div>*/}
       <div
-        className={`group hover:flex-grow py-5 flex justify-end items-end w-40 max-w-[960px] border-2 border-neutral-50 transition-all duration-300 ease-in-out`}
+        className={`group hover:flex-grow py-5 flex justify-end items-end sm:w-40 sm:max-w-[960px] h-[300px] sm:h-full transition-all duration-300 ease-in-out`}
         style={{
           backgroundImage: `url(/ostracon-project-bg.png)`,
           backgroundSize: `auto 100%`,
@@ -23,13 +23,13 @@ export default function SectionProjects() {
         <Link
           href={`https://ostracon.app`}
           target={"_blank"}
-          className={`p-5 w-full bg-neutral-50 opacity-0 group-hover:opacity-100 shadow-lg shadow-neutral-900/30 font-bold text-neutral-900 hover:text-primary text-right uppercase transition-all duration-300`}
+          className={`p-3 sm:p-5 w-full bg-neutral-50 sm:opacity-0 group-hover:opacity-100 shadow-lg shadow-neutral-900/30 font-bold text-neutral-900 hover:text-primary text-right uppercase transition-all duration-300`}
         >
           Visit Ostracon
         </Link>
       </div>
       <div
-        className={`group hover:flex-grow py-5 flex justify-end items-end w-40 max-w-[960px] border-2 border-neutral-50 transition-all duration-300 ease-in-out`}
+        className={`group hover:flex-grow py-5 flex justify-end items-end sm:w-40 sm:max-w-[960px] h-[300px] sm:h-full transition-all duration-300 ease-in-out`}
         style={{
           backgroundImage: `url(/beforeAfter-project-bg.png)`,
           backgroundSize: `auto 100%`,
@@ -39,13 +39,13 @@ export default function SectionProjects() {
         <Link
           href={"https://beforeaftr.me"}
           target={"_blank"}
-          className={`p-5 w-full bg-neutral-50 opacity-0 group-hover:opacity-100 shadow-lg shadow-neutral-900/30 font-bold text-neutral-900 hover:text-primary text-right uppercase transition-all duration-300`}
+          className={`p-3 sm:p-5 w-full bg-neutral-50 sm:opacity-0 group-hover:opacity-100 shadow-lg shadow-neutral-900/30 font-bold text-neutral-900 hover:text-primary text-right uppercase transition-all duration-300`}
         >
           Visit BeforeAftr
         </Link>
       </div>
       <div
-        className={`group hover:flex-grow py-5 flex justify-end items-end w-40 max-w-[960px] border-2 border-neutral-50 transition-all duration-300 ease-in-out`}
+        className={`group hover:flex-grow py-5 flex justify-end items-end sm:w-40 sm:max-w-[960px] h-[300px] sm:h-full transition-all duration-300 ease-in-out`}
         style={{
           backgroundImage: `url(/justnoted-project-bg.png)`,
           backgroundSize: `auto 100%`,
@@ -55,7 +55,7 @@ export default function SectionProjects() {
         <Link
           href={`https://justnoted.app`}
           target={"_blank"}
-          className={`p-5 w-full bg-neutral-50 opacity-0 group-hover:opacity-100 shadow-lg shadow-neutral-900/30 font-bold text-neutral-900 hover:text-primary text-right uppercase transition-all duration-300`}
+          className={`p-3 sm:p-5 w-full bg-neutral-50 sm:opacity-0 group-hover:opacity-100 shadow-lg shadow-neutral-900/30 font-bold text-neutral-900 hover:text-primary text-right uppercase transition-all duration-300`}
         >
           Visit JustNoted
         </Link>
